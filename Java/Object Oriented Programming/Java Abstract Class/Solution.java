80acdc22e3a2a324eeb1412919fe04870cf14a40
Alex Prut <1648497+alexprut@users.noreply.github.com>
2017-10-29 11:58:00 +0100

    Java - Object Oriented Programming

---
+++ Java/Object Oriented Programming/Java Abstract Class/Solution.java
@@ -0,0 +1,6 @@
+//Write MyBook class here
+class MyBook extends Book {
+    void setTitle(String s) {
+        title = s;
+    }
+}
---
+++ Java/Object Oriented Programming/Java Inheritance I/Solution.java
@@ -0,0 +1,12 @@
+
+
+class Bird extends Animal{
+	void fly(){
+		System.out.println("I am flying");
+	}
+
+    void sing(){
+		System.out.println("I am singing");
+	}
+}
+
